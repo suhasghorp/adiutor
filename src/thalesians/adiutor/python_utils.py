9fb084b86c9b88b16ccce008f3237cb79090195d
TonyyyyyTang <tonytzc2005@gmail.com>
2025-04-22 19:07:39 +0100

    Fix get_path_to_scripts for macOS support

--- src/thalesians/adiutor/python_utils.py
+++ src/thalesians/adiutor/python_utils.py
@@ -58,7 +58,8 @@ def get_path_to_python_executable():
     return sys.executable
 
 def get_path_to_scripts():
-    return os.path.abspath(os.path.join(os.path.dirname(get_path_to_python_executable()), 'Scripts'))
+    scripts_dir = 'Scripts' if os.name == 'nt' else 'bin'
+    return os.path.abspath(os.path.join(sys.prefix, scripts_dir))
 
 def _test():
     import doctest
@@ -66,3 +67,4 @@ def _test():
 
 if __name__ == '__main__':
     _test()
+
--- test/test_python_utils.py
+++ test/test_python_utils.py
@@ -12,9 +12,17 @@ class TestPythonUtils(unittest.TestCase):
         
     def test_get_path_to_scripts(self):
         path_to_scripts = python_utils.get_path_to_scripts()
-        self.assertTrue(isinstance(path_to_scripts, str))
-        self.assertTrue(path_to_scripts.endswith('Scripts'))
-        self.assertTrue(os.path.exists(path_to_scripts))
+
+        self.assertTrue(isinstance(path_to_scripts, str), "path_to_scripts is not a string")
+        self.assertTrue(
+            path_to_scripts.endswith('Scripts') or path_to_scripts.endswith('bin'),
+            f"path_to_scripts does not end with 'Scripts' or 'bin': {path_to_scripts}"
+    )
+        self.assertTrue(
+            os.path.exists(path_to_scripts),
+            f"path_to_scripts does not exist: {path_to_scripts}"
+    )
+
         
 if __name__ == '__main__':
     unittest.main()
